d633bd85380cdc00663c398edace2650c041c131
AdminBlog <ryandunderscore2@gmail.com>
2018-06-07 18:47:27 +0000

    Update opoiki.js

--- opoiki.js
+++ opoiki.js
@@ -21,8 +21,8 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
-if(x=="1"){xxx="home-insurance-tips-property-owners-help.html"}
-if(x=="2"){xxx="property-management-tips-for-investment.html"}a_to_ve[i].href="http://www.lewatsini.club/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="how-to-register-perfect-domain-name-for.html"}
+if(x=="2"){xxx="the-best-budget-hosting-for-all-your.html"}a_to_ve[i].href="//www.lewatsini.club/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
