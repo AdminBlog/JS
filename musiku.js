0b796c3d187834a2cf950e249b3cc4b9cf7dda7c
AdminBlog <ryandunderscore2@gmail.com>
2018-11-01 22:30:25 +0700

    Update musiku.js

--- musiku.js
+++ musiku.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
-if(x=="1"){xxx="the-best-budget-hosting-for-all-your.html"}a_to_ve[i].href="http://www.musikhq.site/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="5-cara-menyembunyikan-video-dan-foto-di.html"}a_to_ve[i].href="http://www.viewtekno.com/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
